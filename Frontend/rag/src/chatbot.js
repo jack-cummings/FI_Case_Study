572222b218a6a8f15c28fd1627437f7fefc18bc0
jack-cummings <jmcummi2@ncsu.edu>
2023-11-27 23:21:50 -0500

    highligh and scroll complete

--- Frontend/rag/src/chatbot.js
+++ Frontend/rag/src/chatbot.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect} from 'react';
 
 const Chatbot = () => {
   const [messages, setMessages] = useState([{ text: 'Welcome! How can I help you?', isUser: false }]);
   const [input, setInput] = useState('');
   const [context_window, setContextWindow] = useState('Your Context will apear here, use the chat to continue!')
-  const [documents, setDocuments] = useState([{urls:''}])
+  const [documents, setDocuments] = useState([])
+
+
   // const [isLoading, setIsLoading] = useState(true);
 
   const callAPI = async (question_input) => {
@@ -28,6 +30,47 @@ const Chatbot = () => {
       console.error('Error in POST:', error);
     }}; // end call api
 
+  const StyledSubstring = ({full_text, substring, defaultStyle, substringStyle}) => {
+
+    //  Opperating under the assumption that there is only occurance of substring (only highlights first)
+    let raw 
+
+    if (full_text === 'Your Context will apear here, use the chat to continue!') {
+      raw = full_text.split(substring)
+    }
+    else {
+      raw = full_text.split(substring)
+      raw.splice(1,0,substring)
+      console.log('hit')
+      
+    }
+
+    // console.log(parts)
+    console.log(substring)
+
+    const substringRef = useRef(null);
+
+    useEffect(() => {
+      // Scroll to the substringRef on page load
+      if (substringRef.current) {
+        substringRef.current.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, []);
+
+    return (
+      <p>
+        {raw.map((part, index) => (
+          <span 
+            key={index} 
+            ref={part === substring ? substringRef : null}
+            style={part === substring ? substringStyle : defaultStyle}>
+            {part} 
+            </span>
+          ))}
+        </p>
+      );
+    };
+
 
   const handleInput = (e) => {
     setInput(e.target.value);
@@ -45,8 +88,14 @@ const Chatbot = () => {
       {text: answerObj.content, isUser:false},
     ]);
     setContextWindow(answerObj.context)
-    setDocuments([{urls:answerObj.url}])
-    console.log(documents)
+    // const focus_text = 'A taco'
+    // // const ProcessedContext = await processContext(focus_text,answerObj.context)
+    // const ProcessedContext = await HighlightedSubstring(answerObj.context,focus_text)
+    // setContextWindow(ProcessedContext)
+    setDocuments([
+      ...documents,
+      {urls:answerObj.url}
+    ])
     setInput('');
   };
 
@@ -55,6 +104,7 @@ const Chatbot = () => {
     <div className="layout-container">
       <div className="chatbot">
         {/* Chatbot window */}
+        <h1>Chatbot</h1>
         <div className="chatbot-messages">
           {messages.map((msg, index) => (
             <div key={index} className={msg.isUser ? 'user-message' : 'bot-message'}>
@@ -63,7 +113,7 @@ const Chatbot = () => {
           ))}
         </div>
         <form onSubmit={handleSubmit} className="chatbot-form">
-          <input
+          <textarea
             type="text"
             placeholder="Type a message..."
             value={input}
@@ -75,15 +125,28 @@ const Chatbot = () => {
 
       <div className="middle-panel">
         <h1>Context Window</h1>
-        <div className="scrollable-content">
-          <p>{context_window}</p>
+        <div className="context-window">
+          {/* <p>{context_window}</p> */}
+          <div>
+            <h1>Styled Substring</h1>
+            <StyledSubstring
+              full_text={context_window}
+              substring= 'These instances predate the theory that the first mention of the word "taco" in Mexico was in the 1891 novel Los bandidos de Río Frío by Manuel Payno.'
+              substringStyle={{ fontWeight: 'bold', color: '#009688' }}
+              defaultStyle={{ fontWeight: 'normal', color: 'white' }}
+            />
+          </div>
+
         </div>
       </div>
 
       <div className="file-explorer">
+        <h1>Document History</h1>
       {documents.map((doc, index) => (
-      <div key={index} className='file-object'> {doc.urls}</div>))}
+      <div className='container'><div><a href={doc.urls} key={index} className='file-object'> {doc.urls}</a></div></div>))}
       </div>
+
+      
     </div>
   );
 };
